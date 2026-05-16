d167829d5d7c610c21ff0411610056b5732bfec2
Marc Morant <15051223+marc-mrt@users.noreply.github.com>
2026-05-16 10:58:15 +0200

    update nvim

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,2 +1,21 @@
 require("config.lazy")
+
 vim.cmd('colorscheme tokyonight')
+
+---@type nvim_tree.config
+local config = {
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+}
+
+require("nvim-tree").setup(config)
